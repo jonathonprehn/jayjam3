600ad55dd30c2a0b66f02bd2cf4c28e36f2ff3d6
Jonathon Prehn <horsentp@gmail.com>
2017-11-10 21:05:33 -0600

    Initial commit

---
+++ dbr/src/jonandmatt/dbr/Dbr.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jonandmatt.dbr;
+
+import bropals.lib.simplegame.AWTGameWindow;
+import bropals.lib.simplegame.GameStateRunner;
+import bropals.lib.simplegame.GameWindow;
+import bropals.lib.simplegame.io.AssetManager;
+import java.io.File;
+
+/**
+ *
+ * @author Jonathon
+ */
+public class Dbr {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GameWindow win = new AWTGameWindow("DEATH BUBBLE RACING", 1024, 720, false);
+        AssetManager assets = new AssetManager(new File("assets"), true);
+        GameStateRunner runner = new GameStateRunner(win, assets, new RacingScreen());
+        //start!
+        runner.loop();
+        
+    }
+    
+}
---
+++ dbr/src/jonandmatt/dbr/RacingScreen.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jonandmatt.dbr;
+
+import bropals.lib.simplegame.state.GameState;
+
+/**
+ *
+ * @author Jonathon
+ */
+public class RacingScreen extends GameState {
+
+    @Override
+    public void update(int mills) {
+    }
+
+    @Override
+    public void render(Object graphicsObj) {
+    }
+
+    @Override
+    public void onEnter() {
+    }
+
+    @Override
+    public void onExit() {
+    }
+    
+}
