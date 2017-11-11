3c67b0c904b0b11decd7e4701db3a1156fb5df7a
Matthew-Weiss <matthew.weiss@365.elmhurst.edu>
2017-11-11 10:54:23 -0600

    Added Friction Calc New Position of User and Player Bubbles
    hi

--- dbr/src/jonandmatt/dbr/PlayerBubble.java
+++ dbr/src/jonandmatt/dbr/PlayerBubble.java
@@ -4,31 +4,18 @@
  * and open the template in the editor.
  */
 package jonandmatt.dbr;
-
-import bropals.lib.simplegame.entity.BaseEntity;
 import bropals.lib.simplegame.entity.GameWorld;
-import java.awt.Shape;
 import bropals.lib.simplegame.math.Vector2D;
-import java.awt.Graphics2D;
-
+import jonandmatt.dbr.REntity; 
 /**
  *
- * @author Jonathon
+ * @author Matt Weiss
  */
-public class REntity extends BaseEntity {
-
-    public REntity(GameWorld par) {
-        super(par);
+public class PlayerBubble extends REntity {
+    
+    public PlayerBubble(GameWorld par) {
+        super(null);
     }
-
-    protected RacingScreen state;
-    protected Vector2D pos;
-    protected Vector2D vel;
-    protected double mass;
-    protected double direction; // measured in radians
-    protected java.awt.Shape shape;
-    //private Shape shape;
-    protected Type type;
     
     @Override
     public void update(int mills) {
@@ -37,7 +24,7 @@ public class REntity extends BaseEntity {
         //update my position based on my velocity, and my friction
         Vector2D deltaPositionFromVelocityAndTime = vel.scale((double)mills);
         pos.add(deltaPositionFromVelocityAndTime);
-        /* USE THIS FOR PLAYERBUBBLES
+        // USE THIS FOR PLAYERBUBBLES
         double frictionForce = Surface.getFrictionForce(mySurface, this.mass);
         double userForce = 1.0; // Player.getForce();.....
         direction += -1.0; // Player.getDeltaDirection
@@ -46,31 +33,7 @@ public class REntity extends BaseEntity {
         Vector2D vectorAcceleration = new Vector2D(acceleration * Math.cos(direction), acceleration * Math.sin(direction));
         Vector2D deltaPositionFromAcceleration = vectorAcceleration.scale(0.5 * mills * mills);
          pos.add(deltaPositionFromAcceleration);
-         */
+         
         //oh, am I colliding with something?
-    }
-    
-    // THESE NEED TO BE IN SCOPE OF BOTH THE SET OF ALL BUBBLES AND
-    // AND THE SET OF SURFACES/-- A HIGH SCOPE!
-//
-//    
-//    void updateAllEnvironmentBubblePositions {
-//        
-//    }
-//    void updateAllPlayerBubblesPositions() {
-//        
-//    }
-//    void updateCollisionChecking() {
-//        
-//    }
-    @Override
-    public void render(Object graphicsObj) {
-        Graphics2D g2d = (Graphics2D)graphicsObj;
-        
-        
-    }
-    
-    void giveState(RacingScreen state) {
-        this.state = state;
     }
 }
--- dbr/src/jonandmatt/dbr/Surface.java
+++ dbr/src/jonandmatt/dbr/Surface.java
@@ -11,4 +11,25 @@ package jonandmatt.dbr;
  */
 public enum Surface {
     GRASS, VOID, TRACK, BLOOD;
+    
+    private static double accelerationDueToGravity = 9.81; // m/s^2
+    private static double getMuValue(Surface surface) {
+        switch(surface) {
+            case GRASS:
+                    return 0.5;
+            case VOID:
+                return 100;
+            case TRACK:
+                return 0.1;
+            case BLOOD:
+                return 0.0;
+            default:
+                break;
+        }
+        return 100;
+    }
+    
+    public static double getFrictionForce(Surface surface, double mass) {
+        return getMuValue(surface) * mass * accelerationDueToGravity;
+    }
 }
